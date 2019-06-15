2f602c8871e9bb44b9290511f09f0329f7be4d82
comdet <listzone@hotmail.com>
2019-06-15 16:17:03 +0700

    init

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {    
+    name : 'esp32-doit',
+    platform : 'arduino-esp32',
+    title : 'ESP32 DOIT DEVKIT V1',
+    description : 'ESP32-my Doit.\n',
+    author : 'Comdet Phueadphut',
+    website : 'https://docs.espressif.com/projects/esp-idf/en/latest/get-started/get-started-wrover-kit.html',
+    email : 'comdet.p@gmail.com',
+    git : 'https://github.com/comdet/esp32-doit/',
+    image : '/static/display.jpg',
+    version : '1.0.0'
+};
