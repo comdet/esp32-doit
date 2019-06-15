f11b315c2026275df54c9377a7fafead706db470
comdet <listzone@hotmail.com>
2019-06-15 16:22:05 +0700

    init3

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ module.exports = {
     email : 'comdet.p@gmail.com',
     git : 'https://github.com/comdet/esp32-doit/',
     image : '/static/display.jpg',
-    version : '1.0.1'
+    version : '1.0.2'
 };
